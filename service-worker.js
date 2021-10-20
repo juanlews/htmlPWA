4eda5ffd9df317457f4d59c6b97fa822b27591c5
juanlews <juanlews@hotmail.com>
2021-10-19 22:09:32 -0300

    initial commit

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+var cacheName = 'toferrado-v1.0';
+
+self.addEventListener('install', function (event) {
+  caches.open(cacheName).then((cache) => {
+    cache.addAll([
+      '/',
+      '/index.html',
+      '/manifest.webmanifest',
+      '/coxa.png',
+
+    ]);
+  });
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  let resposta = caches.open(cacheName).then((cache) => {
+    return cache.match(event.request).then((recurso) => {
+      if (recurso) return recurso;
+      return fetch(event.request).then((recurso) => {
+        cache.put(event.request, recurso.clone());
+        return recurso;
+      });
+    });
+  });
+  event.respondWith(resposta);
+});
